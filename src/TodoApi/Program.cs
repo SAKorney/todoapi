4e194e36fb03259b48dce952b205ecfceec1e123
Serge Korneev <sakorneev90@gmail.com>
2026-09-17 12:11:27 +0300

    Simple logging

--- src/TodoApi/Program.cs
+++ src/TodoApi/Program.cs
@@ -26,6 +26,16 @@ builder.Services.AddControllers(options =>
     options.Filters.Add<ValidationFilter>();
 });
 
+if (builder.Environment.IsDevelopment())
+{
+    // Logging settings
+    builder.Services.AddHttpLogging(logging =>
+    {
+        logging.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestPropertiesAndHeaders
+                              | Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.ResponsePropertiesAndHeaders;
+    });
+}
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -36,6 +46,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseHttpLogging();
+
     app.UseDeveloperExceptionPage();
     app.MapOpenApi();
     app.MapScalarApiReference();
--- src/TodoApi/Services/TodoService.cs
+++ src/TodoApi/Services/TodoService.cs
@@ -5,20 +5,23 @@ using TodoApi.Repositories;
 
 namespace TodoApi.Services;
 
-public class TodoService(ITodoRepository repository, IMapper mapper, TimeProvider timeProvider) : ITodoService
+public class TodoService(ITodoRepository repository, ILogger<TodoService> logger, IMapper mapper, TimeProvider timeProvider) : ITodoService
 {
     private readonly ITodoRepository _repository = repository;
+    private readonly ILogger<TodoService> _logger = logger;
     private readonly IMapper _mapper = mapper;
     private readonly TimeProvider _timeProvider = timeProvider;
 
     public async Task<IEnumerable<TodoResponseDto>> GetAllAsync(CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Get All Todos");
         var todos = await _repository.GetAllAsync(cancellationToken);
         return todos.Select(_mapper.Map<TodoResponseDto>);
     }
 
     public async Task<TodoResponseDto?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
     {
+        _logger.LogInformation($"Get todo by id: {id}");
         var todo = await _repository.GetByIdAsync(id, cancellationToken);
         if (todo is null)
         {
@@ -30,24 +33,28 @@ public class TodoService(ITodoRepository repository, IMapper mapper, TimeProvide
 
     public async Task<TodoResponseDto> CreateAsync(CreateTodoDto item, CancellationToken cancellationToken)
     {
+        _logger.LogInformation($"Create todo: {item}");
         var todo = TodoItem.Create(item.Title, _timeProvider.GetUtcNow().UtcDateTime);
         await _repository.AddAsync(todo, cancellationToken);
+        _logger.LogInformation($"Todo {todo.Id} was created");
         return _mapper.Map<TodoResponseDto>(todo);
     }
 
     public async Task<TodoResponseDto?> UpdateAsync(Guid id, UpdateTodoDto item, CancellationToken cancellationToken)
     {
+        _logger.LogInformation($"Update todo {id} : {item}");
         var todo = await _repository.UpdateAsync(id, item.Title, item.IsCompleted, cancellationToken);
         if (todo is null)
         {
             return null;
         }
-
+        _logger.LogInformation($"Todo {todo.Id} was updated");
         return _mapper.Map<TodoResponseDto>(todo);
     }
 
     public async Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken)
     {
+        _logger.LogInformation($"Delete todo {id}");
         return await _repository.DeleteAsync(id, cancellationToken);
     }
 }
