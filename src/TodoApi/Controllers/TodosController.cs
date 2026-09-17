8560031f2e5614878a5cc7a903814c3bb5018025
Serge Korneev <sakorneev90@gmail.com>
2026-09-17 12:15:13 +0300

    Clean Controller from logging

--- src/TodoApi/Controllers/TodosController.cs
+++ src/TodoApi/Controllers/TodosController.cs
@@ -6,15 +6,13 @@ namespace TodoApi.Controllers;
 
 [Route("api/v2/[controller]")]
 [ApiController]
-public class TodosController(ITodoService service, ILogger<TodosController> logger) : ControllerBase
+public class TodosController(ITodoService service) : ControllerBase
 {
     private readonly ITodoService _service = service;
-    private readonly ILogger<TodosController> _logger = logger;
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<TodoResponseDto>>> GetAllAsync(CancellationToken cancellationToken)
     {
-        _logger.LogInformation("Request all todos");
         var response = await _service.GetAllAsync(cancellationToken);
         return Ok(response);
     }
@@ -23,7 +21,6 @@ public class TodosController(ITodoService service, ILogger<TodosController> logg
     [ActionName(nameof(GetByIdAsync))] // Начиная с версии 3.0 среда выполнения по умолчанию удаляет суффикс Async из имен экшенов при генерации маршрутов
     public async Task<ActionResult<TodoResponseDto>> GetByIdAsync(Guid id, CancellationToken cancellationToken)
     {
-        _logger.LogInformation($"Request todo {id}");
         var item = await _service.GetByIdAsync(id, cancellationToken);
         if (item is null)
         {
@@ -35,7 +32,6 @@ public class TodosController(ITodoService service, ILogger<TodosController> logg
     [HttpPost]
     public async Task<IActionResult> CreateAsync(CreateTodoDto item, CancellationToken cancellationToken)
     {
-        _logger.LogInformation($"Request creation: {item}");
         var todo = await _service.CreateAsync(item, cancellationToken);
 
         return CreatedAtAction(nameof(GetByIdAsync), new { id = todo.Id }, todo);
@@ -44,7 +40,6 @@ public class TodosController(ITodoService service, ILogger<TodosController> logg
     [HttpPut("{id}")]
     public async Task<ActionResult<TodoResponseDto>> UpdateAsync(Guid id, UpdateTodoDto item, CancellationToken cancellationToken)
     {
-        _logger.LogInformation($"Request updating: {id}/{item}");
         var updated = await _service.UpdateAsync(id, item, cancellationToken);
         if (updated is null)
         {
@@ -57,7 +52,6 @@ public class TodosController(ITodoService service, ILogger<TodosController> logg
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteAsync(Guid id, CancellationToken cancellationToken)
     {
-        _logger.LogInformation($"Request updating: {id}");
         var deleted = await _service.DeleteAsync(id, cancellationToken);
         if (deleted)
         {
